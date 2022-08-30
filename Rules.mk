1686d57fbd3e1f0b41b0840480a831e42be8d831
iijung <987kmj4@naver.com>
2022-08-30 20:42:55 +0900

    refactor: use built-in functions

--- Rules.mk
+++ Rules.mk
@@ -6,7 +6,7 @@
 #    By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/29 07:07:52 by minjungk          #+#    #+#              #
-#    Updated: 2022/08/30 04:01:40 by minjungk         ###   ########.fr        #
+#    Updated: 2022/08/30 20:42:36 by minjungk         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,14 +28,8 @@ endif
 # function rule 
 # **************************************************************************** #
 link_files:: unlink_files
-	@if [ ! -d $(dst_dir) ]; then \
-		mkdir -p $(dst_dir); \
-	fi;
-	@for file in $(files); do \
-		ln -sf $(src_dir)/$$files $(dst_dir); \
-	done
+	@mkdir -p $(dst_dir);
+	$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir))
 
 unlink_files::
-	@for file in $(files); do \
-		$(RM) $(dst_dir)/$$file; \
-	done
+	$(foreach file,$(files), $(RM) $(dst_dir)/$(file))
