d9c4dfc0d31efe1009478a328db43618101f985c
iijung <987kmj4@naver.com>
2022-10-23 00:33:31 +0900

    refactor: Makefile

--- Rules.mk
+++ Rules.mk
@@ -6,7 +6,7 @@
 #    By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/29 07:07:52 by minjungk          #+#    #+#              #
-#    Updated: 2022/10/16 10:21:13 by minjungk         ###   ########.fr        #
+#    Updated: 2022/10/23 00:04:51 by minjungk         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,7 +33,7 @@ endif
 # **************************************************************************** #
 link_files::
 	@mkdir -p $(dst_dir);
-	$Q$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir);)
+	@$(foreach file,$(files), ln -sf $(src_dir)/$(file) $(dst_dir);)
 
 unlink_files::
-	$Q$(foreach file,$(files), $(RM) $(dst_dir)/$(file);)
+	@$(foreach file,$(files), $(RM) $(dst_dir)/$(file);)
